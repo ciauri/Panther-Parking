6332263cc11fad048dc475a761f49961e171ab74
Stephen Ciauri <stephen.ciauri@gmail.com>
2016-05-04 22:17:39 -0700

    Initial Commit

---
+++ Chapman Parking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chapman Parking
+//
+//  Created by Stephen Ciauri on 5/4/16.
+//  Copyright © 2016 Stephen Ciauri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
