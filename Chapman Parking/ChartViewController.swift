833855c8f4c44eee3346ed43c0fdb4fe8d7f12e3
Stephen Ciauri <stephen.ciauri@gmail.com>
2016-08-04 00:40:45 -0400

    Filled out graph data
    TODO:
    - Enable/Disable lines in graph
    - Make Data -> Grid mapping more efficient
    - Disable map movement, orient in proper position

--- Chapman Parking/ChartViewController.swift
+++ Chapman Parking/ChartViewController.swift
@@ -39,18 +39,21 @@ class ChartViewController: UIViewController {
 //        let dateArray = hourlyDatesInRange(yesterday, endDate: today)
         let chronoSort = NSSortDescriptor(key: "updatedAt", ascending: true)
         request.sortDescriptors = [chronoSort]
-        var colors = Constants.Colors.chartColors
+        var colors = ChartColorTemplates.colorful() + ChartColorTemplates.vordiplom()
+//        var colors = Constants.Colors.chartColors
         
         let formatter = NSDateFormatter()
         formatter.dateStyle = .ShortStyle
         formatter.timeStyle = .MediumStyle
         
+//        let calendar = NSCalendar.currentCalendar()
+        
         
         
         var dataSets: [LineChartDataSet] = []
-        var dates = Set<NSDate>()
+//        var dates = Set<NSDate>()
 //        var indices: [Int] = []
-        var indices = Set<Double>()
+//        var indices = Set<Double>()
 //        var indices = Set<Int>()
         for level in structure.levels!{
             let l = level as! Level
@@ -59,17 +62,34 @@ class ChartViewController: UIViewController {
                 do{
                     let results = try context.executeFetchRequest(request) as! [Count]
                     
+                    let oneMinuteIntervals = self.datesInRange(yesterday, endDate: today, withInterval: 60)
                     var yVals: [ChartDataEntry] = []
-                    for (index, count) in results.enumerate(){
-//                        indices.insert(index)
-//                        count.updatedAt.t
-                        dates.insert(count.updatedAt!)
-                        indices.insert(count.updatedAt!.timeIntervalSinceReferenceDate)
-//                        indices.append(index)
-                        yVals.append(ChartDataEntry(value: count.availableSpaces! as Double, xIndex: index))
+
+                    
+                    var lastCount: Int?
+                    for (index, date) in oneMinuteIntervals.enumerate() {
+                        var countToAdd: Int
+                        if let match = self.countThatMatches(date, withCounts: results) {
+                            countToAdd = Int(match.availableSpaces!)
+                        } else {
+                            if let count = lastCount {
+                                countToAdd = count
+                            } else if let count = self.fetchMostRecentCount(fromDate: date, onLevel: l, usingContext: context){
+                                countToAdd = Int(count.availableSpaces!)
+                            } else {
+                                countToAdd = 0
+                            }
+                            
+                        }
+                        lastCount = countToAdd
+                        yVals.append(ChartDataEntry(value: Double(countToAdd), xIndex: index))
+                        
                     }
+
+                    NSLog("\(yVals.count)")
                     let set = LineChartDataSet(yVals: yVals, label: l.name)
-                    set.circleRadius = 0
+                    set.lineWidth = 3
+                    set.drawCirclesEnabled = false
                     set.colors = [colors.removeFirst()]
                     dataSets.append(set)
                     
@@ -78,16 +98,40 @@ class ChartViewController: UIViewController {
                 }
             })
             
-            NSLog("\(indices.count)")
-            NSLog("\(dataSets.first?.yVals.count)")
-            let dateArray = Array(dates)
-            let timestamps = dateArray.sort({$0.compare($1) == NSComparisonResult.OrderedAscending})
-            let stringStamps = timestamps.map({return formatter.stringFromDate($0)})
+//            NSLog("\(indices.count)")
+//            NSLog("\(dataSets.first?.yVals.count)")
+//            let dateArray = Array(dates)
+//            let timestamps = dateArray.sort({$0.compare($1) == NSComparisonResult.OrderedAscending})
+//            let stringStamps = timestamps.map({return formatter.stringFromDate($0)})
+            let xvals = Array(0...1439)
             
-            lineChart.data = LineChartData(xVals: stringStamps, dataSets: dataSets)
+            lineChart.data = LineChartData(xVals: xvals, dataSets: dataSets)
    
         }
         
+    }
+    
+    private func countThatMatches(date: NSDate, withCounts counts: [Count]) -> Count? {
+        let calendar = NSCalendar.currentCalendar()
+        for count in counts {
+            if calendar.compareDate(date, toDate: count.updatedAt!, toUnitGranularity: .Minute) == .OrderedSame {
+                return count
+            }
+        }
+        return nil
+    }
+    
+    private func fetchMostRecentCount(fromDate date: NSDate, onLevel level: Level, usingContext context: NSManagedObjectContext) -> Count? {
+        let request = NSFetchRequest(entityName: "Count")
+        request.sortDescriptors = [NSSortDescriptor(key: "updatedAt", ascending: false)]
+        request.predicate = NSPredicate(format: "(level == %@) AND (updatedAt <= %@)", level, date)
+        request.fetchLimit = 1
+        do{
+            return try (context.executeFetchRequest(request) as? [Count])?.first
+        } catch {
+            NSLog("error")
+            return nil
+        }
     }
     
     func firstDayOfWeekWithDate(date: NSDate)->NSDate{
@@ -98,13 +142,13 @@ class ChartViewController: UIViewController {
         return beginningOfWeek!
     }
     
-    func hourlyDatesInRange(startDate: NSDate, endDate:NSDate) -> [NSDate]{
-        var secondsBetween = startDate.timeIntervalSinceDate(endDate)
+    func datesInRange(startDate: NSDate, endDate:NSDate, withInterval seconds: Double) -> [NSDate]{
+        var secondsBetween = endDate.timeIntervalSinceDate(startDate)
         var dates: [NSDate] = []
         
         while(secondsBetween > 0){
-            dates.append(startDate.dateByAddingTimeInterval(360))
-            secondsBetween -= 360
+            dates.append(startDate.dateByAddingTimeInterval(secondsBetween))
+            secondsBetween -= seconds
         }
         
         return dates
